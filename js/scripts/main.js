e6b22e66e60c5eab4797738528d18089d2e6ab5b
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-04-19 22:48:50 +0100

    JS for thumb active slide done

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -2,10 +2,13 @@
 var slide_thumbnail = new Swiper(".slide-thumbnail", {
     slidesPerView: 5,
     direction: 'vertical',
-    spaceBetween: 20,
-    
+    spaceBetween: 20, // 20px space between thumbnail
+    watchSlidesProgress: true // check the progress of slide to change accordingly
   });
 
 var slide_hero = new Swiper(".slide-principal", {
-    effect: 'fade' // efeito de fade
+    effect: 'fade', // efeito de fade
+    thumbs: {
+        swiper: slide_thumbnail,
+    }
   });
