a2f726f51c49f64d88d69c4dcd9a1980110959b6
Rui Vergani Neto <ruiverganineto@gmail.com>
2022-05-02 12:04:21 +0100

    js to add and remove active for tab games

--- js/scripts/main.js
+++ js/scripts/main.js
@@ -16,3 +16,24 @@ var slide_hero = new Swiper(".slide-principal", {
       disableOnInteraction: false // autoplay nao para de funcionar
     }
   });
+
+// Games tab to change
+const allFilters = document.querySelectorAll('.js-nav-games li a'); // get the item of list
+const tabPane = document.querySelectorAll('.tab-pane-games');
+
+
+// get the click of the nav for the tabs
+allFilters.forEach((filter, index)=>
+  filter.addEventListener('click', (Event) =>{
+    Event.preventDefault();
+     // remove active from all others
+     allFilters.forEach(item => {
+       item.classList.remove('active');
+     })
+     tabPane.forEach(item => {
+      item.classList.remove('active');
+    })
+     // add active when click
+     tabPane[index].classList.add('active'); // link the index from nav to the tab
+     filter.classList.add('active'); // add active to the nav buttons 
+  }))
